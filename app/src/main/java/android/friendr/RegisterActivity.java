ebee4596b61602c6ecb82b2b953b2fc5c32b9f3a
reshad23 <47149216+reshad23@users.noreply.github.com>
2019-02-14 16:18:27 +0100

    Create RegisterActivity.java

---
+++ app/src/main/java/android/friendr/RegisterActivity.java
@@ -0,0 +1,29 @@
+package com.example.friendr;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private Button buttonToIndex;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        buttonToIndex = (Button)findViewById(R.id.reg_RegisterButtonID);
+
+        buttonToIndex.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View view)
+            {
+                Intent regIntent = new Intent(RegisterActivity.this, Index.class);
+                startActivity(regIntent);
+            }
+        });
+    }
+}
